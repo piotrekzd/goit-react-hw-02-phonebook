108a50acf1e617d000bf95540cca983f07ee7062
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-10-25 10:23:59 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { nanoid } from 'nanoid';
-import style from 'App.module.css';
+import style from './App.module.css';
 
 
 export class App extends React.Component {
